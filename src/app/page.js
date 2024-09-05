001e486f2e74bf26865cab4ecd67f0a7ccec7907
sahilindia12 <indiasahil44@gmail.com>
2024-09-05 07:26:02 -0700

    fdsafsd

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+
+<Link href={'/data'}>go to next</Link>
+<h1>this i shome page</h1>
+
+    </div>
+  )
+}
+
+export default page
